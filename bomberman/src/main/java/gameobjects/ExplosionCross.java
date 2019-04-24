b54eb76a182bbc2a25dba9ac6164f46ac96de8cf
Brian Lai <brianlai30@outlook.com>
2019-04-23 19:55:53 -0700

    Added Transform class and filled out GameObject class

--- bomberman/src/main/java/gameobjects/ExplosionCross.java
+++ bomberman/src/main/java/gameobjects/ExplosionCross.java
@@ -1,11 +1,14 @@
 package gameobjects;
 
+import util.ResourceCollection;
+
 public class ExplosionCross extends GameObject {
 
     ExplosionCross(int power) {
         // power * sprite length * 2 for vertical and horizontal sprite
         // two colliders, one for each
         // center sprite
+        super(ResourceCollection.Images.EXPLOSION.getImage());
     }
 
 }
--- bomberman/src/main/java/gameobjects/GameObject.java
+++ bomberman/src/main/java/gameobjects/GameObject.java
@@ -1,8 +1,76 @@
 package gameobjects;
 
-public abstract class GameObject {
+import util.Transform;
+import util.Vector2D;
 
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
 
+public abstract class GameObject implements CollisionHandling {
+
+    BufferedImage sprite;
+    Transform transform;
+    float width;
+    float height;
+    Vector2D originOffset;
+    Rectangle2D.Double collider;
+
+    private boolean destroyed;
+
+    // Use super() in constructors of subclasses
+    protected GameObject(float xPos, float yPos, float rotation, BufferedImage sprite) {
+        this(sprite);
+        this.transform = new Transform(xPos, yPos, rotation);
+        this.collider = new Rectangle2D.Double(xPos, yPos, this.width, this.height);
+    }
+
+    protected GameObject(BufferedImage sprite) {
+        this.sprite = sprite;
+        this.width = this.sprite.getWidth();
+        this.height = this.sprite.getHeight();
+        this.originOffset = new Vector2D(this.width / 2, this.height / 2);
+    }
+
+    protected void instantiate(GameObject spawnObj, Transform spawnTransform) {
+        float x = spawnTransform.getPositionX() - spawnObj.originOffset.getX();
+        float y = spawnTransform.getPositionY() - spawnObj.originOffset.getY();
+        Vector2D spawnPoint = new Vector2D(x, y);
+        spawnObj.transform.setPosition(spawnPoint);
+        spawnObj.transform.setRotation(spawnTransform.getRotation());
+        spawnObj.collider.setRect(x, y, spawnObj.width, spawnObj.height);
+        // TODO: spawn the object
+    }
+
+    protected void destroy() {
+        this.destroyed = true;
+    }
+
+    public boolean isDestroyed() {
+        return destroyed;
+    }
+
+    /**
+     * Draws the game object in the game world to g.
+     * (ie. the buffer which will be drawn to the screen)
+     * @param g Graphics object that is passed in for the game object to draw to
+     */
+    public void drawImage(Graphics g) {
+        AffineTransform rotation = AffineTransform.getTranslateInstance(this.transform.getPositionX(), this.transform.getPositionY());
+        rotation.rotate(Math.toRadians(this.transform.getRotation()), this.sprite.getWidth() / 2.0, this.sprite.getHeight() / 2.0);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.drawImage(this.sprite, rotation, null);
+    }
+
+    /**
+     * Draw the game object's collider to the game world for debugging.
+     * @param g Graphics object that is passed in for the game object to draw to
+     */
+    public void drawCollider(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.draw(this.collider);
+    }
 
 }
 
